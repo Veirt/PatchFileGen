89636acd4a267f803b2491b451f1c29528ac122f
ExLog <55097092+ExLog@users.noreply.github.com>
2020-10-31 02:17:02 +0800

    Drop to window instead of listwidget

--- PatchFile.py
+++ PatchFile.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QPushButton, QLineEdit
 from PyQt5.QtCore import Qt
 import hashlib
@@ -8,11 +8,28 @@ import shutil
 import re
 
 
-class ListBoxWidget(QListWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+with open("patch_path.txt", "r+") as f:
+    patchPath = f.readline()
+
+
+class PatchGenGUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PatchFileGen")
+        self.setWindowIcon(QtGui.QIcon("miku.png"))
+        self.resize(400, 300)
+        self.line = QLineEdit(self)
+        self.line.move(100, 50)
+        self.line.resize(200, 32)
+        self.line.textChanged.connect(self.on_text_changed)
+
+        self.btn = QPushButton('Patch', self)
+        self.btn.setDisabled(True)
+        self.btn.setGeometry(100, 100, 200, 50)
+        self.btn.clicked.connect(self.makePatch)
+        self.btn.setShortcut("Return")
         self.setAcceptDrops(True)
-        self.resize(400, 30)
+        self.pak = ""
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls:
@@ -31,41 +48,17 @@ class ListBoxWidget(QListWidget):
         if event.mimeData().hasUrls():
             event.setDropAction(Qt.CopyAction)
             event.accept()
-
-            links = []
             for url in event.mimeData().urls():
                 if url.isLocalFile():
-                    links.append(str(url.toLocalFile()))
+                    self.pak = str(url.toLocalFile())
                 else:
-                    links.append(str(url.toString()))
-            self.addItems(links)
+                    self.pak = str(url.toString())
+            self.on_text_changed()
         else:
             event.ignore()
 
-
-class AppDemo(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.resize(400, 300)
-
-        self.listbox_view = ListBoxWidget(self)
-
-        self.line = QLineEdit(self)
-        self.line.move(100, 50)
-        self.line.resize(200, 32)
-        self.line.textChanged.connect(self.on_text_changed)
-
-        self.btn = QPushButton('Patch', self)
-        self.btn.setDisabled(True)
-        self.btn.setGeometry(100, 100, 200, 50)
-        self.btn.clicked.connect(self.makePatch)
-
-    def getSelectedItem(self):
-        item = QListWidgetItem(self.listbox_view.currentItem())
-        return item.text()
-
     def makePatch(self):
-        file_name = self.getSelectedItem()
+        file_name = self.pak
         versionInput = self.line.text()
         if len(versionInput) == 1:
             versionInput = f"00{versionInput}"
@@ -76,14 +69,18 @@ class AppDemo(QMainWindow):
         else:
             sys.exit()
 
-        path = f"00000{versionInput}"
+        path = f"{patchPath}00000{versionInput}"
         try:
             os.mkdir(path)
         except OSError:
             pass
 
-        with open("PatchInfoServer.cfg", "w") as versionCfg:
+        with open(f"{patchPath}PatchInfoServer.cfg", "w") as versionCfg:
             versionCfg.write(f"Version {versionInput}")
+
+        # Make Patch.md5
+        with open(f"{patchPath}00000{versionInput}/Patch00000{versionInput}.pak.md5", "w") as patchMD5:
+            patchMD5.write(f"{hashlib.md5(open(file_name, 'rb').read()).hexdigest()}\n")
 
         # Make Patch.txt file
         with open(file_name, "rb") as pak:
@@ -92,27 +89,25 @@ class AppDemo(QMainWindow):
             output_decoded = map(lambda decoded: f"D {decoded}", decoded)
             output_txt = "\n".join(list(output_decoded))
 
-        with open(f"00000{versionInput}/Patch00000{versionInput}.txt", "w") as patchTxt:
+        with open(f"{patchPath}00000{versionInput}/Patch00000{versionInput}.txt", "w") as patchTxt:
             patchTxt.write(output_txt)
 
-        # Make Patch.md5
-        with open(f"00000{versionInput}/Patch00000{versionInput}.pak.md5", "w") as patchMD5:
-            patchMD5.write(f"{hashlib.md5(open(file_name, 'rb').read()).hexdigest()}\n")
-
         try:
-            shutil.copy2(file_name, f"00000{versionInput}/Patch00000{versionInput}.pak")
+            shutil.copy2(file_name, f"{patchPath}00000{versionInput}/Patch00000{versionInput}.pak")
         except shutil.SameFileError:
             pass
 
+        sys.exit()
+
     @QtCore.pyqtSlot()
     def on_text_changed(self):
-        self.btn.setEnabled(bool(self.line.text()))
+        self.btn.setEnabled(bool(self.line.text()) and bool(self.pak != ""))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    demo = AppDemo()
-    demo.show()
+    MainGUI = PatchGenGUI()
+    MainGUI.show()
 
     sys.exit(app.exec_())
